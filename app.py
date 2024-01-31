97ebb5a828b0f62e40a37b846956a0a4927eb1cb
AlexanderJohnRobertson <s275931@uos.ac.uk>
2024-01-31 01:45:39 +0000

    added containerisation

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ from sqlite3 import Error
 #from flask_sessionstore import Session
 #from flask_session_captcha import FlaskSessionCaptcha
 #from pymongo import MongoClient
+import os
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your secret key'
 
@@ -570,5 +571,8 @@ def deleteproductssuccess():
 def logout():
     return render_template('logout.html')
 
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
